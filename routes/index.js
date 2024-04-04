fe2db25b1644ea38eee77b4fbc575c58e5da43f1
anduhrooo <yang.andrew7@gmail.com>
2024-04-03 22:32:56 -0700

    added routes to display user info on profile, expenses on landing

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,4 @@ router.use('/users', userRoutes);
 const expensesRoutes = require('./expensesRoutes')
 router.use('/expenses', expensesRoutes)
 
-router.get('/', async (req, res) => {
-    res.render('expenses');
-})
-
-// router.get('/users', async (req, res) => {
-//     res.render('profile');
-// })
-
 module.exports = router;
