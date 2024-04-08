4517c541385110bcea10834c3caf14e76ede76e3
KeanuFord <KeanuF2017@gmail.com>
2024-04-08 13:03:39 -0700

    Correctly displays expenses from logged in user

--- public/js/expenses.js
+++ public/js/expenses.js
@@ -23,39 +23,3 @@ window.addEventListener("click", function(event) {
     closeModal();
   }
 });
-
-const hbs = exphbs.create({
-  helpers: {
-      select: function (context, index, options) {
-          var ret = "";
-          ret = ret + options.fn(context[index]);
-          return ret;
-      },
-      selectAll: function (context, options) {
-          var num;
-          var ret = 0;
-          for (var i = 0, j = context.length; i < j; i++) {
-              num = parseInt(options.fn(context[i]));
-              ret = ret + num;
-              console.log("ret: " + ret);
-              console.log("num: " + num);
-              console.log("context: " + options.fn(context[i]));
-          }
-          return ret;
-      },
-      // total: function (context, options) {
-      //     var ret = 0;
-      //     for (var i = 0, j = context.length; i < j; i++) {
-      //         ret = ret + options.fn(context[i])
-      //     }
-      //     return ret;
-      // },
-      list: function (context, options) {
-          var ret = "";
-          for (var i = 0, j = context.length; i < j; i++) {
-              ret = ret + options.fn(context[i]);
-          }
-          return ret;
-      }
-  }
-});
